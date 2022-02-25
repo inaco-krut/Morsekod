38154cd43a898c75b81a15238b76713ea7c75aa7
inaco <qlB1m3ApASJt>
2022-02-25 23:47:57 +0100

    v1.6

--- src/MainKlass.java
+++ src/MainKlass.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MainKlass {
     public static void main(String[] args) {
+try {
 
         while (true) {
             System.out.println("press 1 to convert from text to morse\npress 2 to convert from morse to English");
@@ -13,15 +14,18 @@ public class MainKlass {
                         System.out.println("Write the text you want to translate");
                         ConvertToMorse textTillMorse = new ConvertToMorse();
                         System.out.println(textTillMorse);
-
                     case 2:
                         System.out.println("Write the morse code you want to translate, add a space between each morse code");
                         ConvertToText MorseTillText = new ConvertToText();
                         Scanner scn = new Scanner(System.in);
                         System.out.println(MorseTillText.getConverterToText(scn.nextLine()));
                         break;
+                        default:
+                        System.out.println("not an option, try again");
                     }
                 }
-            }
+            }catch (Exception e){
+                System.out.println("wrong character, use 1 or 2");
         }
-
+    }
+}
